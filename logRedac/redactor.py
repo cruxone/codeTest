6bbc6b711d897502c5e93a0214efd4a93f36b9a1
Chris Freeland <diathis47@gmail.com>
2017-06-06 18:59:47 -0700

    made pseudo log file, basic logic for redactor.

--- logRedac/redactor.py
+++ logRedac/redactor.py
@@ -1,6 +1,22 @@
 import sys
+import os
+
+def openFile(filePath):
+    try:
+        f = open(filePath, 'r')
+        contents = f.readlines()
+        for line in contents:
+            print(line)
+    except:
+        print("error reading file")
 
 def main():
-    print("called")
+    param = sys.argv
+    try:
+        if param[1]:
+            print("Argument supplied: {}".format(param[1]))
+            openFile("logData.txt")
+    except:
+        print("Incorrect parameters")
 
 main()
