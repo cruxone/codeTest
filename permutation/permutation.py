5f14c76c65b4a1a3b59d1614f49d58f41e16e765
Chris Freeland <diathis47@gmail.com>
2017-06-06 17:46:46 -0700

    added base logic for permutations challenge.

--- permutation/permutation.py
+++ permutation/permutation.py
@@ -1,16 +1,26 @@
 
 import sys
 
-def perm(toPerm):
-    for i in range(len(toPermargLength)):
-        print(i)
+def permutate(toPerm):
+    #check if length of string is viable for iteration, if so return list
+    if len(toPerm) <= 1:
+        return [toPerm]
+    # grab first letter of string
+    firstChar = toPerm[0]
+    print(firstChar)
+    # grab all letters after first string
+    permutations = permutate(toPerm[1:])
+    print(permutations)
 
 def main():
     args = sys.argv
-    try:
-        if args[1]:
-            perm(args[1])
-    except:
-        print("No argument given")
+    print(permutate('LSE'))
+    # try:
+    #     if args[1]:
+    #         print("yus")
+    #         perm(args[1])
+    #
+    # except:
+    #     print("No argument given")
 
 main()
