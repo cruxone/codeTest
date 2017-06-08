31723925fec80f366b1694b71e1f2ce0abef453d
Chris Freeland <diathis47@gmail.com>
2017-06-08 16:08:24 -0700

    dummy gz file added for testing. Copy, gunzip, and filtering works. Now testing findall() rather than search().

--- logRedac/redactor.py
+++ logRedac/redactor.py
@@ -5,8 +5,9 @@ import re
 
 
 def copyFile(srcFile):
-    dstFile = srcFile + '.redact'
+    dstFile = srcFile + '.redact.gz'
     shutil.copy2(srcFile, dstFile)
+    print("New file is: {}".format(dstFile))
     try:
         gunzipFile(dstFile)
         print("copy successful")
@@ -16,18 +17,32 @@ def copyFile(srcFile):
 def gunzipFile(log):
     # will gunzip file, and then call redact with gunzipped file.
     print("gunzip called")
+    regex = r"\d{3}-\d{2}-\d{4}"
     counter = 0
     redactCount = 0
-    with gzip.open(log, 'rb') as f:
-        for line in f.readlines():
+    openFile = gzip.open(log, 'rb')
+    contents = openFile.readlines()
+    openFile.close()
+    openFile = gzip.open(log, 'w')
+    for line in contents:
+        socialMatch = re.search(regex, line)
+        if not socialMatch:
+            openFile.write(line)
+        else:
             counter = counter + 1
-            socialMatch = re.search("Fred", line)
-            #socialReplace = re.sub(EX, 'xxx-xxx-xxxx', line)
-            if socialMatch:
-                redactCount = redactCount + 1
-                print("Fred found")
-        print(redactCount)
-        print(counter)
+    print("sensitive data found: {} times.".format(counter))
+    #openFile.close()
+
+    # with gzip.open(log, 'rb') as f:
+    #     for line in f.readlines():
+    #         counter = counter + 1
+    #         socialMatch = re.search("Fred", line)
+    #         #socialReplace = re.sub(EX, 'xxx-xxx-xxxx', line)
+    #         if socialMatch:
+    #             redactCount = redactCount + 1
+    #             print("Fred found")
+    #     print(redactCount)
+    #     print(counter)
 #def redact(line, counter):
     #will redact documents and log
     # FOR LOGGING: log name of file redacted, total lines, and lines redacted
@@ -36,12 +51,9 @@ def gunzipFile(log):
 
 def main():
     param = sys.argv
-    copyFile(param[1])
     try:
         if param[1]:
-            print("Argument supplied: {}".format(param[1]))
-            # copy file
-
+            copyFile(param[1])
     except:
         print("Incorrect parameters")
 
