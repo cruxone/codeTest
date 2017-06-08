245cf0291aa1929d5a8c44c83f35d5e4fe2ef611
Chris Freeland <diathis47@gmail.com>
2017-06-07 18:03:24 -0700

    getting skeleton of program fleshed out, leaving openFile for now, will be moved into redactFile.

--- logRedac/redactor.py
+++ logRedac/redactor.py
@@ -1,12 +1,39 @@
 import sys
-import os
+import gzip
+import re
+
+
+def copyFile():
+    #called by main(), will copy file then send new file to gunzipFile()
+    print("cp called")
+    logToRedact = "test"
+    gunzipFile(logToRedact)
+
+def gunzipFile(log):
+    # will gunzip file, and then call redact with gunzipped file.
+    print("gunzip called")
+    redact()
+
+def redact():
+    #will redact documents and log
+    # FOR LOGGING: log name of file redacted, total lines, and lines redacted
+    print("redact called")
 
 def openFile(filePath):
     try:
         f = open(filePath, 'r')
         contents = f.readlines()
+        count = 0
+        #print(contents)
         for line in contents:
-            print(line)
+
+            socialMatch = re.search("Fred", line)
+            #socialReplace = re.sub(EX, 'xxx-xxx-xxxx', line)
+            #print(line)
+            if socialMatch:
+                count = count + 1
+                #print(socialMatch.group())
+        print(count)
     except:
         print("error reading file")
 
@@ -15,7 +42,8 @@ def main():
     try:
         if param[1]:
             print("Argument supplied: {}".format(param[1]))
-            openFile("logData.txt")
+            # copy file
+            copyFile(param[1])
     except:
         print("Incorrect parameters")
 
