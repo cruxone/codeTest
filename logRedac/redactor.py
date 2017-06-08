a813016a2ecc0625f8629cc88f42e9b07d870809
Chris Freeland <diathis47@gmail.com>
2017-06-07 18:45:48 -0700

    copy file and gunzip read works.

--- logRedac/redactor.py
+++ logRedac/redactor.py
@@ -1,49 +1,47 @@
 import sys
 import gzip
+import shutil
 import re
 
 
-def copyFile():
-    #called by main(), will copy file then send new file to gunzipFile()
-    print("cp called")
-    logToRedact = "test"
-    gunzipFile(logToRedact)
+def copyFile(srcFile):
+    dstFile = srcFile + '.redact'
+    shutil.copy2(srcFile, dstFile)
+    try:
+        gunzipFile(dstFile)
+        print("copy successful")
+    except:
+        print("failed to copy")
 
 def gunzipFile(log):
     # will gunzip file, and then call redact with gunzipped file.
     print("gunzip called")
-    redact()
-
-def redact():
-    #will redact documents and log
-    # FOR LOGGING: log name of file redacted, total lines, and lines redacted
-    print("redact called")
-
-def openFile(filePath):
-    try:
-        f = open(filePath, 'r')
-        contents = f.readlines()
-        count = 0
-        #print(contents)
-        for line in contents:
-
+    counter = 0
+    redactCount = 0
+    with gzip.open(log, 'rb') as f:
+        for line in f.readlines():
+            counter = counter + 1
             socialMatch = re.search("Fred", line)
             #socialReplace = re.sub(EX, 'xxx-xxx-xxxx', line)
-            #print(line)
             if socialMatch:
-                count = count + 1
-                #print(socialMatch.group())
-        print(count)
-    except:
-        print("error reading file")
+                redactCount = redactCount + 1
+                print("Fred found")
+        print(redactCount)
+        print(counter)
+#def redact(line, counter):
+    #will redact documents and log
+    # FOR LOGGING: log name of file redacted, total lines, and lines redacted
+    #print("redact called", counter)
+
 
 def main():
     param = sys.argv
+    copyFile(param[1])
     try:
         if param[1]:
             print("Argument supplied: {}".format(param[1]))
             # copy file
-            copyFile(param[1])
+
     except:
         print("Incorrect parameters")
 
