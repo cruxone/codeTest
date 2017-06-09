9eeea9787a7718f4f9ef35ae396faad88b010ccf
Chris Freeland <diathis47@gmail.com>
2017-06-09 01:01:46 -0700

    basic logging function implemented. gzip read has been slightly optimized, gzip.open for copy file is still slow.

--- logRedac/redactor.py
+++ logRedac/redactor.py
@@ -2,56 +2,53 @@ import sys
 import gzip
 import shutil
 import re
+import io
 
 
-def copyFile(srcFile):
-    dstFile = srcFile + '.redact.gz'
-    # copying source file, copies permissions and most metadata
-    shutil.copy2(srcFile, dstFile)
-    print("New file is: {}".format(dstFile))
-    try:
-        gunzipFile(dstFile)
-        print("copy successful")
-    except:
-        print("failed to copy")
-
-def gunzipFile(log):
+def gunzipFile(srcFile):
     # will gunzip file, and then call redact with gunzipped file.
     # regex for SSN
-    regex = r"\d{3}-\d{2}-\d{4}"
-    # open file in read mode to grab contents
-    openFile = gzip.open(log, 'rb')
-    print("opened " + log)
-    contents = openFile.readlines()
-    # close read file
-    openFile.close()
-    # open file in write mode
-    openFile = gzip.open(log, 'w')
-    # sort through contents
-    counter = 0
-    for line in contents:
-        # if the search term is not found, the line will be written to the new file.
-        # This takes care of duplicates in the same line as well.
-        socialMatch = re.search(regex, line)
-        if not socialMatch:
-            openFile.write(line)
-        else:
-            counter = counter + 1
-    print("sensitive data found: {} times.".format(counter))
-    # close write mode file
-    openFile.close()
-    print("closed " + log)
+    regex = re.compile(b"\d{3}-\d{2}-\d{4}")
+    print("opened " + srcFile)
+    dstFile = srcFile + '.redact.gz'
+    totalCount = 0
+    redactCount = 0
+    gunzipped = gzip.open(srcFile, 'rb')
+    f = io.BufferedReader(gunzipped)
+    with gzip.open(dstFile, 'wb') as r:
+        #with gzip.open(dstFile, 'wb') as r:
+        for line in f:
+            # if the search term is not found, the line will be written to the new file.
+            totalCount = totalCount + 1
+            socialMatch = re.search(regex, line)
+            if not socialMatch:
+                r.write(line)
+            else:
+                redactCount = redactCount + 1
+    print("closed " + srcFile)
+    logger(srcFile, totalCount, redactCount)
+
+
+def logger(srcFile, totalCount, redactCount):
+# name of each file processed, a count of the total number of lines in log file, count of the total number of lines redacted
+# The audit log may additionally contain any other information you feel is pertinent
+# The audit log must not contain any information from the redacted lines.
+    with open("redact.log", 'a+') as logFile:
+        logFile.write("File redacted: {}\n".format(srcFile))
+        logFile.write("Total lines: {}\n".format(totalCount))
+        logFile.write("Lines redacted: {}\n-----------------------\n".format(redactCount))
+
+
 
 def main():
     param = sys.argv
-    try:
-        directory = param[1]
-        count = 0
-        filesToFilter = param[2:]
-        for item in filesToFilter:
-            toProcess = directory + item
-            copyFile(toProcess)
-    except:
-        print("Incorrect parameters")
+    #try:
+    directory = param[1]
+    filesToFilter = param[2:]
+    for item in filesToFilter:
+        toProcess = directory + item
+        gunzipFile(toProcess)
+    #except:
+    #    print("Incorrect parameters")
 
 main()
